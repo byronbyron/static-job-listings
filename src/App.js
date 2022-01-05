e654c7bae14446588db02c4d95c09debd4b94095
Byronn Walker-Mills <bwalkermills@gmail.com>
2022-01-05 14:32:18 +0000

    Add remove filter functionality

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import ListingsData from './data/data.json';
 
 export default function App() {
   const [listings, setListings] = useState([]);
-  const [filters, setFilter] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
-    setListings(ListingsData)
-  }, []);
+    if(filters.length == 0)
+      setListings(ListingsData)
+  }, [filters]);
 
   const filterListings = (filter_by = '') => {
     if (!filters.includes(filter_by) && filter_by !== '') {
-      setFilter([...filters, filter_by]);
+      setFilters([...filters, filter_by]);
 
       setListings(listings.filter(listing => [
         listing.role,
@@ -25,13 +26,17 @@ export default function App() {
     }
   }
 
+  const removeFilter = (removed_filter) => {
+    setFilters(filters.filter(item => item !== removed_filter))
+  }
+
   return (
     <div>
       <Header />
 
       <main className="container mx-auto px-6">
         <div className="filter">
-          { filters.map( filter => <FilterItem key={filter} item={filter} /> ) }
+          { filters.map(filter => <FilterItem item={filter} _callback={removeFilter} key={filter} />)}
         </div>
 
         <div className="card-group">
--- src/components/FilterItem.js
+++ src/components/FilterItem.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-export default function FilterItem({item, callback}) {
+export default function FilterItem({item, _callback}) {
+  const removeFilter = () => {
+    _callback(item)
+  }
+
   return (
-    <button className="text-xs p-2 bg-white rounded cursor-pointer mr-2">
+    <button className="text-xs p-2 bg-white rounded cursor-pointer mr-2" onClick={() => removeFilter()}>
       {item}
     </button>
   )
